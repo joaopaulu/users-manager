f8e110acf90164d93b96bea91569666e850f5462
João Paulo <jptick@gmail.com>
2022-07-17 15:38:32 -0300

    Implementing User Services

--- frontend/src/app/service/user.service.ts
+++ frontend/src/app/service/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Page } from '../interface/page';
+import { ApiResponse } from './../interface/api-response';
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +12,12 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(
+  users$ = (
     name: string = '',
     page: number = 0,
     size: number = 10
-  ): Observable<any> {
-    return this.http.get<any>(
+  ): Observable<ApiResponse<Page>> =>
+    this.http.get<ApiResponse<Page>>(
       `${this.serverUrl}?name=${name}&page=${page}&size=${size}`
     );
-  }
 }
